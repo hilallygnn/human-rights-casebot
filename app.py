61937dbe0ea62c9de48ca21afd812ec0cba2bad9
hilallygnn <hilalyegen@anadolu.edu.tr>
2025-10-22 16:16:46 +0300

    Update app.py

--- app.py
+++ app.py
@@ -55,9 +55,8 @@ def setup_rag_pipeline():
     RAG_PROMPT_TEMPLATE = PromptTemplate(template=template, input_variables=["context", "question"])
 
     # 2.6 RAG Chain (RetrievalQA) Setup
-   llm = ChatGoogleGenerativeAI(model="gemini-1.5-flash")
+    llm = ChatGoogleGenerativeAI(model="gemini-1.5-flash")
 
-    
     qa_chain = RetrievalQA.from_chain_type(
         llm=llm,
         chain_type="stuff",
